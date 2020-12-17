ac986125ab8abee08619fc80c36bf7159add764c
Pimski26 <p.w.j.terietmole@students.uu.nl>
2020-12-17 14:26:19 +0100

    Tidying code
    Removed square brackets from function output
    
    Removed default arguments when initiating ModelResult instance

--- +lib/+costs/total_deaths.m
+++ +lib/+costs/total_deaths.m
@@ -1,4 +1,4 @@
-function [nr_of_deaths] = total_deaths(M,l)
+function nr_of_deaths = total_deaths(M,l)
 %TOTAL_DEATHS 
 % This function takes two variables as its input, M and l
 % M is an instance of the ModelResult class
--- +tests/+lib/+costs/total_deaths_test.m
+++ +tests/+lib/+costs/total_deaths_test.m
@@ -7,15 +7,7 @@ l = [0.00022274,0.00022274,0.00022274,0.00022274,0.00022274,0.00221398,0.0135161
 
 % Run model
 % Call the lib.SIRV_model.
-M = lib.SIRV_model(1, ... The timestap DeltaT
-    'StartDate', '2020-10-09', ... The date for which the data should be loaded.
-    'Steps', 160 , ... The amount of steps to calculate beforehand.
-    'Method', "EulerForward", ... The numeric method to solve the ODE of the model.
-    'AgeGroups', 0:10:70, ... The way the age groups should be devided.
-    'ContactMatrixFile', "Contact_matrix.csv", ... The file that contains the contact matrix.
-    'Tau', 5, ... The average time of an infection.
-    'ReprNum', 5.0 ... Set the reproduction number to a fixed value.
-);
+M = lib.SIRV_model();
 
 %% Calculate total deaths
 lib.costs.total_deaths(M,l)
